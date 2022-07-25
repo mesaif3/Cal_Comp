3e2bef3653397ab757febd90119dd43e60b09b2e
safe <elgalaly.3@gmail.com>
2022-07-25 23:42:20 +0300

    testing caching

--- app.py
+++ app.py
@@ -32,18 +32,10 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
-# Ensure responses aren't cached
-@app.after_request
-def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
-
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = True
+app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
@@ -66,7 +58,7 @@ def index():
     # returns a combined calendar
     # session["session_id"] = 1243
     # session["session_name"] = "test"
-    # session["people"] = get_people()
+    session["people"] = get_people()
     # if not session.get("active_people",None):
     #     session["active_people"] = list(session["people"].values())
 
