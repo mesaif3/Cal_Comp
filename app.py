617a12f143713b3d3f577dd1d43b6d09511b042a
mesaif3 <68240863+mesaif3@users.noreply.github.com>
2023-08-02 22:06:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,6 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# postgres://qszjzfyfycymmb:b8e925ef3948e3573262de3898981adb988c357159ecc20de19d11f842ff84bd@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/d1ugqh51mo7jpt
 # Configure CS50 Library to use SQLite database
 uri = os.getenv("DATABASE_URL")
 if uri.startswith("postgres://"):
